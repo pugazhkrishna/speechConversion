120a9fc00716cd2cf8def6e0223c133e5a952b83
Pugazh Krishna <sridevibagavathi@gmail.com>
2022-02-13 23:21:30 +0530

    elastic beanslk setup

--- app.js
+++ app.js
@@ -1,63 +1,41 @@
-const express = require('express');
-// const fs = require('fs');
-// const multer = require('multer');
-// const upload = multer();
-// const { promisify } = require('util');
-// const { v4 } = require('uuid');
-const cors = require('cors')
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-// const writeFile = promisify(fs.writeFile);
-// const readdir = promisify(fs.readdir);
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-const app = express();
+var app = express();
 
-app.use(cors({
-    origin: '*'
-  }));
-  app.use(cors({
-    methods: '*'
-  }));
-// make sure messages folder exists
-// const messageFolder = './public/messages/';
-// if (!fs.existsSync(messageFolder)) {
-//   fs.mkdirSync(messageFolder);
-// }
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use(express.static('public'));
-// app.use(express.json());
-app.get('/',(req,res)=>{
-  res.send("Hello world")
-})
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
-// app.get('/messages', (req, res) => {
-//   readdir(messageFolder)
-//     .then(messageFilenames => {
-//       res.status(200).json({ messageFilenames });
-//     })
-//     .catch(err => {
-//       console.log('Error reading message directory', err);
-//       res.sendStatus(500);
-//     });
-// });
-
-// app.post('/messages', (req, res) => {
-//   if (!req.body.message) {
-//     return res.status(400).json({ error: 'No req.body.message' });
-//   }
-//   const messageId = v4();
-//   writeFile(messageFolder + messageId, req.body.message, 'base64')
-//     .then(() => {
-//       res.status(201).json({ message: 'Saved message' });
-//     })
-//     .catch(err => {
-//       console.log('Error writing message to file', err);
-//       res.sendStatus(500);
-//     });
-// });
-
-// app.use(express.static(__dirname));
-const PORT = process.env.PORT || 5001;
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
